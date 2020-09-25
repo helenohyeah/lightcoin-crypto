7918eefe689a440c5653451e37834486a0a38a8a
helenohyeah <ouyang.helen@gmail.com>
2020-09-25 14:22:52 +0000

    Refactored Deposit commit to a getter method

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ class Withdrawal extends Transaction {
 // creates a transaction obj to allow a user to deposit money to their balance
 class Deposit extends Transaction {
 
-  //  finalize and apply the transaction to the account's balance
-  commit() {
-    console.log(this);
-    this.account.balance += this.amount;
+  // finalize and apply the transaction to the account's balance
+  // returns new balance
+  get value() {
+    return this.account.balance += this.amount;
   }
 
 }
@@ -44,15 +44,16 @@ class Deposit extends Transaction {
 const myAccount = new Account("snow-patrol");
 
 t1 = new Deposit(500, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+t1.value;
+console.log('Transaction 1:', t1.amount);
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
 
-t3 = new Withdrawal(125.25, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+// t3 = new Withdrawal(125.25, myAccount);
+// t3.commit();
+// console.log('Transaction 3:', t3);
 
 console.log('Balance:', myAccount.balance);
+console.log(myAccount);

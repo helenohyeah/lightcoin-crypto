5401eb6f79326d47dd18b26609a354bbccc4d2c8
helenohyeah <ouyang.helen@gmail.com>
2020-09-25 14:32:25 +0000

    Fixed refactoring based on hints from Compass

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ class Transaction {
     this.account = account;
   }
 
+  // finalize and apply the transaction to the account's balance
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 // allow a user to withdraw money from their balance
 class Withdrawal extends Transaction {
 
-  // finalize and apply the transaction to the account's balance
-  // returns new balance
+  // returns value of change to the balance
   get value() {
-    return this.account.balance -= this.amount;
+    return -this.amount;
   }
 
 }
@@ -33,10 +36,9 @@ class Withdrawal extends Transaction {
 // allow a user to deposit money to their balance
 class Deposit extends Transaction {
 
-  // finalize and apply the transaction to the account's balance
-  // returns new balance
+  // returns value of change to the balance
   get value() {
-    return this.account.balance += this.amount;
+    return this.amount;
   }
 
 }
@@ -46,15 +48,15 @@ class Deposit extends Transaction {
 const myAccount = new Account("snow-patrol");
 
 t1 = new Deposit(500.00, myAccount);
-t1.value;
+t1.commit();
 console.log('Transaction 1:', `$${t1.amount}`);
 
 t2 = new Withdrawal(9.99, myAccount);
-t2.value;
+t2.commit();
 console.log('Transaction 2:', `$${t2.amount}`);
 
 t3 = new Withdrawal(125.25, myAccount);
-t3.value;
+t3.commit();
 console.log('Transaction 3:', `$${t3.amount}`);
 
 console.log('Balance:', `$${myAccount.balance}`);
